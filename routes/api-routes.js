9efc4418b94347e1c11084465439c024993bf779
Lauren Doss <doss.laurene@gmail.com>
2020-04-25 12:48:01 -0700

    including updated api-routes to match local

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,51 +3,62 @@ var db = require("../models");
 
 
 module.exports = function(app) {
-  // Using the passport.authenticate middleware with our local strategy.
-  // If the user has valid login credentials, send them to the members page.
-  // Otherwise the user will be sent an error
-  // app.post("/api/login", passport.authenticate("local"), function(req, res) {
-  //   // Sending back a password, even a hashed password, isn't a good idea
-  //   res.json({
-  //     email: req.user.email,
-  //     id: req.user.id
-  //   });
-  // });
 
-  // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
-  // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
-  // otherwise send back an error
-  app.post("/api/signup", function(req, res) {
-    db.User.create({
-      email: req.body.email,
-      password: req.body.password
-    })
-      .then(function() {
-        res.redirect(307, "/api/login");
-      })
-      .catch(function(err) {
-        res.status(401).json(err);
-      });
+  app.post("/api/drinks", function(req, res) {
+    console.log(req.body);
+    db.drink.create({
+    drinkName: req.body.drinkName, 
+    ingOne: req.body.ingOne, 
+    ingOneOz: req.body.ingOneOz,
+    ingTwo: req.body.ingTwo,
+    ingTwoOz: req.body.ingTwoOz,
+    ingThree: req.body.ingThree,
+    ingThreeOz: req.body.ingThreeOz,
+    ingFour: req.body.ingFour,
+    ingFourOz: req.body.ingFourOz,
+    drinkNotes: req.body.drinkNotes
+    }).then(function(dbDrink) {
+      res.json(dbDrink);
+    });
   });
 
-  // Route for logging user out
-  app.get("/logout", function(req, res) {
-    req.logout();
-    res.redirect("/");
+
+  app.get("/api/drinks", function(req, res) {
+    db.drink.findAll({}).then(function(dbDrink){
+      res.json(dbDrink); 
+    }); 
   });
 
-  // Route for getting some data about our user to be used client side
-  app.get("/api/user_data", function(req, res) {
-    if (!req.user) {
-      // The user is not logged in, send back an empty object
-      res.json({});
-    } else {
-      // Otherwise send back the user's email and id
-      // Sending back a password, even a hashed password, isn't a good idea
-      res.json({
-        email: req.user.email,
-        id: req.user.id
-      });
-    }
+  app.delete("/api/drinks/:id", function(req, res) {
+    db.drink.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbDrink) {
+      res.json(dbDrink);
+    });
+
   });
+  app.put("/api/drinks", function(req, res) {
+    db.drink.update({
+      drinkName: req.body.drinkName, 
+      ingOne: req.body.ingOne, 
+      ingOneOz: req.body.ingOneOz,
+      ingTwo: req.body.ingTwo,
+      ingTwoOz: req.body.ingTwoOz,
+      ingThree: req.body.ingThree,
+      ingThreeOz: req.body.ingThreeOz,
+      ingFour: req.body.ingFour,
+      ingFourOz: req.body.ingFourOz,
+      drinkNotes: req.body.drinkNotes
+    }, {
+      where: {
+        id: req.body.id
+      }
+    }).then(function(dbDrink) {
+      res.json(dbDrink);
+    });
+  });
+
+ 
 };
