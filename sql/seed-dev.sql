00896dd26149cfbe877829c5f14345cee05f5e79
dylan@dylanhulbert.com <dylan@dylanhulbert.com>
2020-04-30 23:32:33 -0700

    updated seed

---
+++ sql/seed-dev.sql
@@ -0,0 +1,17 @@
+USE drinkDB;
+
+INSERT INTO drink (drinkName, ingOne, ingOneOz, ingTwo, ingTwoOz, ingThree, ingThreeOz, ingFour, ingFourOz, drinkNotes) VALUES 
+("Vodka Soda", "Vodka", "3", "Club Soda", "7", "Lime", "1", "", "", "Serve on ice"),
+("Mojito", "Rum", "3", "Lime Juice", "1", "Soda water", "4", "Mint", "1", "Serve fresh"),
+("Dry Martini", "Gin", "2", "Dry Vermouth", "1", "Olive", "1", "", "", "Delicious Classic"),
+("Old Fashioned", "Bourbon", "4", "Angostura Bitters", "1", "Sugar", ".2", "", "", "Garnish with orange twist, and a cocktail cherry"),
+("Margarita", "Tequila", "2", "triple Sec", ".5", "lime Juice", "1", "Salt", ".1", "Classic"),
+("Long Island Tea", "Vodka", "2", "Light Rum", "2", "Light Gin", "2", "Tequila", "2", "Delicious choice"),
+("Whiskey Sour", "Blended Whiskey", "2", "Lemon Juice", ".5", "", "", "", "", "Serve with slice of lemon and one cherry"),
+("Moscow Mule", "Vodka", 2, "Lime Juice", 2, "Ginger Ale", 8, "", "", "Serve in copper mug"),
+("Daiquiri", "Light Rum", "2", "Lime Juice", "2", "Powdered Sugar", ".1", "", "", "Popular choice"),
+("Gin And Tonic", "Gin", "2", "Tonic Water", "5", "", "", "", "", "Serve with lime"),
+("Vodka Redbull", "Red Bull", "8", "Vodka", "2", "", "", "", "", "Club drink"),
+("Negroni", "Gin", "1", "Campari", "1", "Sweet Varmouth", "1", "", "", "Stir into glass over ice, garnish and serve"),
+("747", "Vodka", "1", "Roses Sweetened Lime juice", "1", "Cranberry Juice", "1", "", "",  "Top with sprite"),
+("White Wine Sangria", "Lime", "1", "Lemon", "1", "White Wine", "2", "Strawberries", "1", "Top with soda water");
