8209898cd1efcd6271ff6305e6dd6aae33f39ab7
dylan@dylanhulbert.com <dylan@dylanhulbert.com>
2020-05-02 10:01:28 -0700

    name change

---
+++ public/js/recentSearch.js
@@ -0,0 +1,8 @@
+var recentDrink = $("button#recentAdd");
+recentDrink.on("click", function(event){
+  event.preventDefault();
+  window.location.href="recent.html";
+//  for (i=0; i < newDrink.length; i++){
+//    console.log(newDrink.length);
+//  }
+})
